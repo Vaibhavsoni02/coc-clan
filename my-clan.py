ec6668335f78561507298000cc389009dd4e0f5a
Vaibhav Soni <vaibhavgear7@gmail.com>
2023-08-12 20:16:05 +0530

    Create my-clan.py

---
+++ my-clan.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import requests
+import json
+
+# Function to fetch data from the Clash of Clans API
+def fetch_data():
+    url = "https://api.clashofclans.com/v1/clans/%23RGRPVQUJ"
+    headers = {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjI5NWZiNjkzLTUwN2EtNDQ1NS04MGYwLTZjZjYxMGUyZjcxYyIsImlhdCI6MTY5MTg1MDIwMSwic3ViIjoiZGV2ZWxvcGVyL2Q3ZGMwZTFiLTQwM2YtOTY1NC0xZDM2LTBmZGY4NjA2MTc1MCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjM1LjIzMC41OC4yMTEiXSwidHlwZSI6ImNsaWVudCJ9XX0.fp5f0HjUagNzwjDChqJ2veO9Da9RgPfl7Hjcyj80TW5eUyrsGW0Z8u8bVBL-tp24XOifnpr5TiBL5AptM8mh-Q'
+    }
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
+
+# Fetching clan data
+clan_data = fetch_data()
+
+# Extracting member details
+members = clan_data['memberList']
+member_details = [[
+    member['tag'],
+    member['name'],
+    member['role'],
+    member['expLevel'],
+    member['trophies'],
+    member['builderBaseTrophies'],
+    member['donations']
+] for member in members]
+
+# Title of the dashboard
+st.title('Clash of Clans Clan Dashboard')
+
+# Metrics
+st.subheader('Metrics')
+st.write('Total Members:', clan_data['members'])
+st.write('Clan Points:', clan_data['clanPoints'])
+st.write('Clan Level:', clan_data['clanLevel'])
+
+# Member details table
+st.subheader('Member Details')
+st.table(
+    member_details,
+    columns=['Tag', 'Name', 'Role', 'Exp Level', 'Trophies', 'Builder Base Trophies', 'Donations']
+)
